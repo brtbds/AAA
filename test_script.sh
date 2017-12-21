07cb4bed8d44d618d6638018e5e4aff648ad8920
mosewa <brtbds@yandex.ru>
2017-12-21 10:13:48 +0300

    3.3 - test scripts

---
+++ test_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "\"1\" \"2\" \"5\""
+bash "$1" "1" "2" "3"
+echo ' '
+
+echo "five random args"
+bash "$1" $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM
+
+echo
+echo "\foo\" \"bar\" \"foobar\" \"foo bar\""
+bash "$1" "foo" "bar" "foobar" "foo bar"
+
+echo
+echo "\"foo\" \"--foo\" \"--help\" \"-l\""
+bash "$1" "foo" "--foo" "--help" "-l"
